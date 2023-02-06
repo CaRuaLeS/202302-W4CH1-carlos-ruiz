3620e98e86b6ac5e9fc08e07966fb445a359c5f1
CarlosRuiz <carua97@gmail.com>
2023-02-06 20:31:54 +0100

    Add components with ERROR

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Info } from "../info/info";
-import "./style.css";
 
 function App() {
   return (
     <>
-      <header className="main-header">
-        <h1 className="main-title">The pointing gentlemen</h1>
-      </header>
-      <Info></Info>
+      <div className="container">
+        <header className="main-header">
+          <h1 className="main-title">The pointing gentlemen</h1>
+        </header>
+        <Info></Info>
+      </div>
     </>
   );
 }
--- src/components/info/info.tsx
+++ src/components/info/info.tsx
@@ -1,8 +1,31 @@
+import { GENTLEMAN } from "../../mocks/mock-gentleman";
+import { Button } from "../button/button";
+import { Gentleman } from "../gentleman/gentleman";
+
 export function Info() {
   return (
-    <section className="controls">
-      <p className="info">0 gentlemen pointing at you</p>
-      <button className="button button--select">Select all</button>
-    </section>
+    <>
+      {" "}
+      <section className="controls">
+        <p className="info">0 gentlemen pointing at you</p>
+        <Button></Button>
+      </section>
+      <main className="main">
+        <ul className="gentlemen">
+          {GENTLEMAN.map((item) => (
+            <Gentleman
+              id={item.id}
+              name={item.name}
+              status={item.status}
+              profession={item.profession}
+              twitter={item.twitter}
+              picture={item.picture}
+              alternativeText={item.alternativeText}
+              selected={item.selected}
+            ></Gentleman>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
