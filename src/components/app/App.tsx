7277646ae76090e192d6795d77025d53c7db40fc
CarlosRuiz <carua97@gmail.com>
2023-02-06 19:02:26 +0100

    creating info component and adding css (on process)

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,25 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Info } from "../info/info";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
       </header>
-    </div>
+      <Info></Info>
+    </>
   );
 }
 
